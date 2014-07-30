9435a9db051aac3b59f047e99dc164f6e43059f6
lee212 <hroe.lee@gmail.com>
2014-07-30 15:11:04 -0400

    minor

--- cm_metric/shell/Constants.py
+++ cm_metric/shell/Constants.py
@@ -6,5 +6,9 @@ class CMConst:
     DEFAULT_NIMBUS_DB = "sqlite3"
     DEFAULT_OPENSTACK_VERSION = "essex"
 
-    DEFAULT_CONFIG_FILENAME = "cloudmesh.cfg"
-    DEFAULT_CONFIG_FILEPATH = os.getenv("HOME") + "/.cloudmesh/"
+    # We still use old names but at some point, we will replace it with
+    # cloudmesh - July 30, 2014
+    #DEFAULT_CONFIG_FILENAME = "cloudmesh.cfg"
+    DEFAULT_CONFIG_FILENAME = "futuregrid.cfg"
+    DEFAULT_CONFIG_FILEPATH = os.getenv("HOME") + "/.futuregrid/"
+    #DEFAULT_CONFIG_FILEPATH = os.getenv("HOME") + "/.cloudmesh/"
